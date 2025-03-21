1877bea497c41414f9639b5de20f21e5fa8df2c2
Jerry Mathew <jer2ymatheww@gmail.com>
2025-03-21 19:17:55 +0530

    working on functionality

--- index.js
+++ index.js
@@ -10,28 +10,51 @@ function uuid() {
 let todoInput = document.querySelector('.input')
 let addTodoButton = document.querySelector('.button')
 let showTodos = document.querySelector('.todos-container')
-let todo;
+let todo=''
 let todoList = []
 
 
 addTodoButton.addEventListener('click', function(event) {
     event.preventDefault()
     todo = todoInput.value
-    if(todo){
+    if(todo.length > 0) {
         todoList.push({
-            id: uuid(),
             todo: todo,
+            id: uuid(),
             isCompleted:false
         })
-    }
     renderTodoList(todoList)
+    todoInput.value = ''
+    }
 })
 
-showTodos.addEventListener('click', ()=>{
-    console.log('clicked');
+showTodos.addEventListener('click', (e)=>{
+    e.preventDefault()
+    let key = e.target.dataset.key;
+    let delTodoKey = e.target.dataset.todokey;
+    todoList = todoList.map((todo) =>
+        todo.id === key ? {
+            ...todo,
+            isCompleted: !todo.isCompleted
+        } : todo
+    );
+    todoList = todoList.filter((todo) => todo.id !== delTodoKey);
+    console.log(todoList);
+    renderTodoList(todoList);
     
 })
 
-function renderTodoList(todoList){
-    showTodos.innerHTML = todoList.map(({ id,todo,isCompleted}) => `<div><input id='item-${id}' type='checkbox'><label for='item-${id}' class ='todo'>${todo}</label><button>Delete</button></div>`)
+function renderTodoList(todoList) {
+    showTodos.innerHTML = todoList.map(
+        ({
+            todo,
+            id,
+            isCompleted
+        }) => `<div class="todo relative"> <input id="item-${id}" data-key=${id} class="t-checkbox t-pointer" type="checkbox" ${
+        isCompleted ? "checked" : ""
+      }> <label data-key=${id} class="todo-text t-pointer ${
+        isCompleted ? "checked-todo" : ""
+      }" for="item-${id}"> ${todo} </label> <button class="absolute right-0 button cursor">
+      <span data-todokey=${id}  class="del-btn material-icons-outlined">delete</span>
+            </button> </div>`)
 }
